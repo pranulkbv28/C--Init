8f3615151a308e16533d6bd740e393837344ba5b
pranulkbv28 <28bvpranaav@gmail.com>
2024-10-17 23:25:24 +0530

    inital commit

---
+++ MyFirstProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 2;
+
+            Console.WriteLine(num);
+
+            Console.ReadLine();
+        }
+    }
+}
